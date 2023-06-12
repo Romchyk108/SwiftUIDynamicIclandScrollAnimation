8847911c6a35d843172f0d38a9447625fb56ae08
Roman Shestopal <romchyk.r@gmail.com>
2023-06-12 21:29:55 +0300

    Initial Commit

---
+++ DynamicIslandScrollAnimation/DynamicIslandScrollAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandScrollAnimationApp.swift
+//  DynamicIslandScrollAnimation
+//
+//  Created by Roman Shestopal on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandScrollAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
