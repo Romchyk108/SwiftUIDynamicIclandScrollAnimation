de2ce460cb7b784453404842c76ff7a7ee1f8238
Roman Shestopal <romchyk.r@gmail.com>
2023-12-26 11:11:39 +0200

    first commit

---
+++ DynamicIslandScrollAnimation/AnchorKey.swift
@@ -0,0 +1,20 @@
+//
+//  AnchorKey.swift
+//  DynamicIslandScrollAnimation
+//
+//  Created by Roman Shestopal on 13.06.2023.
+//
+
+import SwiftUI
+
+struct AnchorKey: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnchorKey_Previews: PreviewProvider {
+    static var previews: some View {
+        AnchorKey()
+    }
+}
